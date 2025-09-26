3ff7c40351523f014ea7000113bf30cc60a57084
Arshina Dange <c203344@ardentmills.com>
2025-09-26 22:49:28 +0530

    nav bar changeas

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ class NavBar extends Component {
     state = {}
     render() {
         return (
-            <nav className="navbar bg-body-tertiary">
+            <nav className="navbar " style={{ backgroundColor: "#001f3f" }}>
                 <div className="container-fluid">
-                    <a className="navbar-brand">My Hotel App</a>
+                    <a className="navbar-brand text-white p-4 fw-bold">Home Stay</a>
                     <form className="d-flex" role="search">
                         <input
                             className="form-control me-2"
