6cef820a0a07934054e1a3be863c88cc8b096df5
Nirmal <nabram@dentalxchange.com>
2025-09-26 23:50:54 +0530

    Admin and page format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
 import HotelList from "./components/HotelList";
 import { useEffect, useState } from "react";
-import Footer from "./components/Footer";
 import LoginPage from "./components/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/sideBar";
 import axios from "axios";
-
+import Layout from "./pages/layout";
+import HomeLayout from "./pages/homeLayout";
+import AdminPage from "./pages/admin";
 function App() {
   const hotel = [
     {
@@ -73,10 +72,10 @@ function App() {
           name: hotel.hotel_name,
           address: hotel.address,
           timezone: hotel.timezone,
-          price: 10000,
+          price: Math.floor(Math.random() * 10000),
           roomsBooked: 0,
           image:
-            hotel._id % 2 == 0
+            Math.floor(Math.random() * 10000) % 2 == 0
               ? "/assets/le-meridien.webp"
               : "/assets/itc-chola.webp",
         }));
@@ -126,34 +125,50 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />}></Route>
         <Route
-          path="/home"
           element={
-            <>
-              <NavBar></NavBar>
-              <main className="d-flex justify-content-between mx-3 flex-row w-100 screen-height">
-                <span className="d-flex flex-column col-10 overflow-auto">
-                  <HotelList
-                    hotelList={hotelList}
-                    incrementRoomsBooked={incrementRoomsBooked}
-                    decreamentRoomsBooked={decreamentRoomsBooked}
-                  />
-                </span>
-                <hr className="border  opacity-50 screen-height mb-3" />
-                <Sidebar
-                  className="col-2 position-sticky"
-                  totalAmount={totalAmount}
-                  roomCount={totalRooms}
-                />
-              </main>
-              <Footer totalAmount={totalAmount} resetData={resetData}></Footer>
-            </>
+            <Layout
+              totalAmount={totalAmount}
+              roomCount={totalRooms}
+              resetData={resetData}
+            />
           }
-        ></Route>
+        >
+          <Route
+            path="/"
+            element={
+              <HomeLayout
+                totalAmount={totalAmount}
+                roomCount={totalRooms}
+                resetData={resetData}
+              />
+            }
+          >
+            <Route
+              path="/home"
+              element={
+                <HotelList
+                  hotelList={hotelList}
+                  incrementRoomsBooked={incrementRoomsBooked}
+                  decreamentRoomsBooked={decreamentRoomsBooked}
+                />
+              }
+            />
+          </Route>
+          <Route
+            path="/admin"
+            element={
+              <AdminPage
+                hotelList={hotelList}
+                incrementRoomsBooked={incrementRoomsBooked}
+                decreamentRoomsBooked={decreamentRoomsBooked}
+              />
+            }
+          />
+        </Route>
+        <Route path="/" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
